d7dcd3ede0a4eeeadeed0a59689425ccee19e4dd
J.J. Jackson <jj@expectlabs.com>
2017-05-17 16:27:21 -0700

    BP1: Update parser config for new format (#6)

--- blueprints/food_ordering/config.py
+++ blueprints/food_ordering/config.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """This module contains the Food Ordering Blueprint Application"""
 
-
 PARSER_CONFIG = {
-    'dish': [{'type': 'option', 'max_instances': None}, {'type': 'quantity', 'right': False}],
-    'option': [{'type': 'quantity', 'right': False}],
-    'restaurant': [],
-    'cuisine': [],
-    'category': []
+    'dish': {
+        'option': {'linking_words': {'with'}},
+        'quantity': {'max_instances': 1, 'right': False}
+    },
+    'option': {
+        'quantity': {'max_instances': 1, 'right': False}
+    }
 }
 
 CLASSIFIER_CONFIGS = {
