37ae65ed74f28c2034dd352375d5b641d996a7e3
Juan Rodriguez <angel.neb@gmail.com>
2017-06-26 15:42:45 -0500

    Created placeholders for all states

--- blueprints/video_discovery/app.py
+++ blueprints/video_discovery/app.py
@@ -11,41 +11,13 @@ def welcome(context, slots, responder):
     """
     When the user starts a conversation, say hi.
     """
-    try:
-        # Get user's name from session information in request context to personalize the greeting.
-        slots['name'] = context['request']['session']['name']
-        prefix = 'Hello, {name}. '
-    except KeyError:
-        prefix = 'Hello. '
-
-    # Build up the final natural language response and reply to the user.
-    responder.prompt(prefix + 'How are you doing? Want to watch some movies?')
+    responder.reply("greet placeholder.")
 
 
-@app.handle(intent='exit')
-def say_goodbye(context, slots, responder):
-    """
-    When the user ends a conversation, clear the dialogue frame and say goodbye.
-    """
-    # Clear the dialogue frame to start afresh for the next user request.
-    context['frame'] = {}
-
-    # Respond with a random selection from one of the canned "goodbye" responses.
-    responder.reply(['Bye!', 'Goodbye!', 'Have a nice day.', 'See you later.'])
-
-
-@app.handle(intent='help')
-def provide_help(context, slots, responder):
-    """
-    When the user asks for help, provide some sample queries they can try.
-    """
-    # Respond with examples demonstrating how the user can search for movies.
-    # For simplicity, we have a fixed set of demonstrative queries here, but they could also be
-    # randomly sampled from a pool of example queries each time.
-    prompts = ["I can help you find movies that you might be interested. For example, "
-               "you can say 'I would like some comedy movies' or 'I feel like "
-               "to watch movies with Tom Hanks.'"]
-    responder.prompt(prompts)
+@app.handle(intent='browse')
+def show_content(context, slots, responder):
+    # Show the video content based on the entities found.
+    responder.reply("browse placeholder.")
 
 
 @app.handle(intent='start_over')
@@ -53,21 +25,57 @@ def start_over(context, slots, responder):
     """
     When the user wants to start over, clear the dialogue frame and prompt for the next request.
     """
-    # Clear the dialogue frame and respond with a variation of the welcome message.
-    context['frame'] = {}
-    prompts = ["Ok, let's start over! What movies do you want to watch?"]
-    responder.prompt(prompts)
+    responder.reply("start_over placeholder.")
+
+
+@app.handle(intent='exit')
+def say_goodbye(context, slots, responder):
+    """
+    When the user ends a conversation, clear the dialogue frame and say goodbye.
+    """
+    responder.reply("exit placeholder.")
+
+
+@app.handle(intent='help')
+def provide_help(context, slots, responder):
+    """
+    When the user asks for help, provide some sample queries they can try.
+    """
+    responder.reply("help placeholder.")
 
 
 @app.handle(intent='unsupported')
+def handle_unsupported(context, slots, responder):
+    # Respond with a message explaining the app does not support that # query. 
+    responder.reply("unsupported placeholder.")
+
+
+@app.handle(intent='unrelated')
+def handle_unrelated(context, slots, responder):
+    # Respond with a message explaining the app does not support that # query. 
+    responder.reply("unrelated placeholder.")
+
+
+@app.handle(intent='compliment')
+def say_something_nice(context, slots, responder):
+    # Respond with a compliment or something nice.
+    responder.reply("compliment placeholder.")
+
+
+@app.handle(intent='insult')
+def handle_insult(context, slots, responder):
+    # Evade the insult and come back to the app usage.
+    responder.reply("insult placeholder.")
+
+
 @app.handle()
 def default(context, slots, responder):
     """
     When the user asks an unrelated question, convey the lack of understanding for the requested
     information and prompt to return to video discovery.
     """
-    prompts = ['Sorry, not sure what you meant there. I can help you find some good movies.']
-    responder.prompt(prompts)
+    responder.reply("default placeholder.")
+
 
 
 if __name__ == '__main__':
