82e2b884d63561f00f0a702d6f5f3619a9ab0e90
alexhua <alhuang10@gmail.com>
2017-06-27 15:18:25 -0700

    More PR comments

--- blueprints/home_assistant/app.py
+++ blueprints/home_assistant/app.py
@@ -2,11 +2,14 @@
 """This module contains the Workbench home assistant blueprint application"""
 from __future__ import unicode_literals
 from mmworkbench import Application
-from urllib.request import urlopen
-import json
+import requests
 import os
 
 app = Application(__name__)
+
+CITY_NOT_FOUND_CODE = '404'
+INVALID_API_KEY_CODE = '401'
+
 
 @app.handle(intent='check-weather')
 def check_weather(context, slots, responder):
@@ -19,28 +22,26 @@ def check_weather(context, slots, responder):
     except:
         reply = "Open weather API is not setup, please follow instructions to setup the API."
         responder.reply(reply)
-
-    # Check for user's requested city and temperature unit
-    location_entity = next((e for e in context['entities'] if e['type'] == 'city'), None)
-    unit_entity = next((e for e in context['entities'] if e['type'] == 'unit'), None)
+        return
 
     # Get the location the user wants
-    selected_location = _get_location(location_entity)
-    # Figure out which unit the user wants information in
-    selected_unit = _get_unit(unit_entity)
+    selected_location = _get_location(context)
+    # Figure out which temperature unit the user wants information in
+    selected_unit = _get_unit(context)
 
     # Get weather information via the API
     url_string = _construct_weather_api_url(selected_location, selected_unit, openweather_api_key)
     try:
-        weather_info = json.load(urlopen(url_string))
+        weather_info = requests.get(url_string).json()
     except:
         reply = "Sorry, I was unable to connect to the weather API, please check your connection."
         responder.reply(reply)
+        return
 
-    if weather_info['cod'] == '404':
+    if weather_info['cod'] == CITY_NOT_FOUND_CODE:
         reply = "Sorry, I wasn't able to recognize that city."
         responder.reply(reply)
-    elif weather_info['cod'] == '401':
+    elif weather_info['cod'] == INVALID_API_KEY_CODE:
         reply = "Sorry, the API key is invalid."
         responder.reply(reply)
     else:
@@ -57,6 +58,7 @@ def default(context, slots, responder):
     prompts = ["Sorry, not sure what you meant there."]
     responder.prompt(prompts)
 
+
 def _construct_weather_api_url(selected_location, selected_unit, openweather_api_key):
     base_string = "http://api.openweathermap.org/data/2.5/weather"
     api_key_string = "&appid=" + openweather_api_key
@@ -70,41 +72,43 @@ def _construct_weather_api_url(selected_location, selected_unit, openweather_api
     url_string = base_string + api_key_string + unit_key_string + location_string
     return url_string
 
-def _get_unit(unit_entity):
+
+def _get_unit(context):
     """
     Get's the user desired temperature unit from the query, defaulting to Fahrenheit if none provided
 
     Args:
-        unit_entity (entity):
+        unit_entity (dict): a unit entity with two possible resolved values (Celcius and Fahrenheit)
 
     Returns:
         string: resolved temperature unit entity
     """
+    unit_entity = next((e for e in context['entities'] if e['type'] == 'unit'), None)
+
     if unit_entity:
-        selected_unit = app.question_answerer.get(index='units', id=unit_entity['value'][0]['id'])[0]
+        return app.question_answerer.get(index='units', id=unit_entity['value'][0]['id'])[0]
     else:
         # Default to Fahrenheit
-        selected_unit = 'Fahrenheit'
+        return 'Fahrenheit'
 
-    return selected_unit
 
-def _get_location(location_entity):
+def _get_location(context):
     """
     Get's the user location from the query, defaulting to San Francisco if none provided
 
     Args:
-        location_entity (entity):
+        location_entity (dict): a location entity with potentially many candidate resolved locations
 
     Returns:
         string: resolved location entity
     """
-    if location_entity:
-        selected_location = app.question_answerer.get(index='cities', id=location_entity['value'][0]['id'])[0]
-    else:
-        #
-        selected_location = 'San Francisco'
+    location_entity = next((e for e in context['entities'] if e['type'] == 'city'), None)
 
-    return selected_location
+    if location_entity:
+        return app.question_answerer.get(index='cities', id=location_entity['value'][0]['id'])[0]
+    else:
+        # Default to San Francisco
+        return 'San Francisco'
 
 if __name__ == '__main__':
     app.cli()
