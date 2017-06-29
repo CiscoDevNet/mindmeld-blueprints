2757a7e289e22210aa440649e4f129c8044e85c6
Ray Wang <r790822@gmail.com>
2017-06-29 12:13:32 -0700

    Check in initial model config for faster iteration

--- blueprints/video_discovery/config.py
+++ blueprints/video_discovery/config.py
@@ -4,7 +4,45 @@
 PARSER_CONFIG = {
 }
 
-CLASSIFIER_CONFIGS = {
+INTENT_MODEL_CONFIG = {
+    'model_type': 'text',
+    'model_settings': {
+        'classifier_type': 'logreg'
+    },
+    'params': {
+        'C': 10,
+        "class_bias": 0.3
+    },
+    'features': {
+        "bag-of-words": {
+            "lengths": [1, 2]
+        },
+        "edge-ngrams": {"lengths": [1, 2]},
+        "in-gaz": {},
+        "exact": {"scaling": 10},
+        "gaz-freq": {},
+        "freq": {"bins": 5}
+    }
+}
+
+DOMAIN_MODEL_CONFIG = {
+    'model_type': 'text',
+    'model_settings': {
+        'classifier_type': 'logreg'
+    },
+    'params': {
+        'C': 10,
+    },
+    'features': {
+        "bag-of-words": {
+            "lengths": [1, 2]
+        },
+        "edge-ngrams": {"lengths": [1, 2]},
+        "in-gaz": {},
+        "exact": {"scaling": 10},
+        "gaz-freq": {},
+        "freq": {"bins": 5}
+    }
 }
 
 APP_NAME = 'video_discovery'
