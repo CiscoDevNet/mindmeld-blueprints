bb1c4d02896b3d0944e4da4954f2ea3507e62ce2
Ray Wang <r790822@gmail.com>
2017-06-30 14:34:12 -0700

    Check in config for entity model

--- blueprints/video_discovery/config.py
+++ blueprints/video_discovery/config.py
@@ -4,6 +4,30 @@
 PARSER_CONFIG = {
 }
 
+ENTITY_MODEL_CONFIG = {
+    'model_type': 'memm',
+    'model_settings': {
+        'tag_scheme': 'IOB',
+        'feature_scaler': 'max-abs'
+    },
+    'params': {
+        'penalty': 'l2',
+        'C': 10000
+    },
+    'features': {
+        'bag-of-words-seq': {
+            'ngram_lengths_to_start_positions': {
+                1: [-2, -1, 0, 1, 2],
+                2: [-2, -1, 0, 1]
+            }
+        },
+        'in-gaz-span-seq': {},
+        'sys-candidates-seq': {
+            'start_positions': [-1, 0, 1]
+        }
+    }
+}
+
 INTENT_MODEL_CONFIG = {
     'model_type': 'text',
     'model_settings': {
