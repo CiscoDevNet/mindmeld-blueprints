5373ac51d6ff4eab86a08c9ba8ff8e99dfec691b
Ray Wang <r790822@gmail.com>
2017-07-03 15:07:26 -0700

    Update import kb script

--- blueprints/video_discovery/scripts/kb/load_to_kb.sh
+++ blueprints/video_discovery/scripts/kb/load_to_kb.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 HOST=dev-search3-001.dev
-APP_NAME=video_discovery
-INDEX_NAME=20170627
-FILENAME=output_data_0626_1k/transformed/video_1k.jsonl
+APP_NAME=ray_test_video_discovery
+INDEX_NAME=20170702
 
 
-mmworkbench load-kb -n $HOST $APP_NAME $INDEX_NAME $FILENAME
+DATA_DIR=data/output_data-0629-1900/transformed
+
+FILENAME=transformed_tvs.jsonl
+time mmworkbench load-kb -n $HOST $APP_NAME $INDEX_NAME $DATA_DIR/$FILENAME
+
+FILENAME=transformed_movies.jsonl
+time mmworkbench load-kb -n $HOST $APP_NAME $INDEX_NAME $DATA_DIR/$FILENAME
