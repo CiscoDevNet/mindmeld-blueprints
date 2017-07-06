290a5ea172de43f3d425cf23b8c2d75522573699
Jingyu Li <jingyuli@mit.edu>
2017-07-06 10:36:36 -0700

    a few tests, fixed gazetteer

--- blueprints/video_discovery/tests/functional/test_frame.py
+++ blueprints/video_discovery/tests/functional/test_frame.py
@@ -19,3 +19,39 @@ class TestFrame(ConversationTest):
             'genre': [{'type': 'genre', 'text': 'action'}]
         }
         self.assert_frame(convo, expected)
+
+    def test_comedy_tv(self):
+        convo = Conversation(app_path=APP_PATH)
+        convo.say('i want to watch a funny show')
+        expected = {
+            'type': [{'type': 'type', 'text': 'show'}],
+            'genre': [{'type': 'genre', 'text': 'funny'}]
+        }
+        self.assert_frame(convo, expected)
+
+    def test_comedy_tv(self):
+        convo = Conversation(app_path=APP_PATH)
+        convo.say('i want to watch a funny show')
+        expected = {
+            'type': [{'type': 'type', 'text': 'show'}],
+            'genre': [{'type': 'genre', 'text': 'funny'}]
+        }
+        self.assert_frame(convo, expected)
+
+    def test_jennifer_aniston_movie(self):
+        convo = Conversation(app_path=APP_PATH)
+        convo.say('i want to watch a movie starring Jennifer Aniston')
+        expected = {
+            'type': [{'type': 'type', 'text': 'movie'}],
+            'cast': [{'type': 'cast', 'text': 'Jennifer Aniston'}]
+        }
+        self.assert_frame(convo, expected)
+
+    def test_canadian_movie(self):
+        convo = Conversation(app_path=APP_PATH)
+        convo.say('i want to watch a Canadian movie')
+        expected = {
+            'type': [{'type': 'type', 'text': 'movie'}],
+            'country': [{'type': 'country', 'text': 'Canadian'}]
+        }
+        self.assert_frame(convo, expected)
