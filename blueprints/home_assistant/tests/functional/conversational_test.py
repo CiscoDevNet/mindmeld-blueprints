26c4a51dc4991f27bd3b2f43a2ba47efc2125892
Minh-Tue Vo <mvo@edx.org>
2017-07-06 10:54:08 -0700

    HA-22: Adding tests for weather (#35)
    1) Small refactor for conversational tests
    2) Add weather tests

--- blueprints/home_assistant/tests/functional/conversational_test.py
+++ blueprints/home_assistant/tests/functional/conversational_test.py
@@ -1,3 +1,25 @@
+def load_conversation():
+    import os
+    app_path = os.path.dirname(os.path.realpath(__file__)).replace('tests/functional', '')
+
+    from mmworkbench.components import NaturalLanguageProcessor
+    nlp = NaturalLanguageProcessor(app_path=app_path)
+    nlp.load()
+
+    from mmworkbench import Conversation
+    return Conversation(nlp=nlp, app_path=app_path)
+
+
 class ConversationalTest(object):
-    def assert_text(self, texts, expected_text):
+    @classmethod
+    def setup_class(cls):
+        cls.conv = load_conversation()
+
+    @staticmethod
+    def assert_text(texts, expected_text):
         assert texts[0] == expected_text
+
+    @staticmethod
+    def assert_intent(conv, expected_intent):
+        last_history = conv.history[-1]
+        assert last_history['intent'] == expected_intent
--- blueprints/home_assistant/tests/functional/test_unknown.py
+++ blueprints/home_assistant/tests/functional/test_unknown.py
@@ -1,28 +1,7 @@
-import pytest
 from conversational_test import ConversationalTest
 
 
-@pytest.fixture()
-def app_path():
-    import os
-    return os.path.dirname(os.path.realpath(__file__)).replace('tests/functional', '')
-
-
-@pytest.fixture()
-def nlp(app_path):
-    from mmworkbench.components import NaturalLanguageProcessor
-    nlp = NaturalLanguageProcessor(app_path=app_path)
-    nlp.load()
-    return nlp
-
-
-@pytest.fixture()
-def conv(nlp, app_path):
-    from mmworkbench import Conversation
-    return Conversation(nlp=nlp, app_path=app_path)
-
-
 class TestUnknown(ConversationalTest):
-    def test_unknown(self, conv):
-        texts = conv.say("what was the soviet union?")
+    def test_unknown(self):
+        texts = self.conv.say("what was the soviet union?")
         self.assert_text(texts, "Sorry, not sure what you meant there.")
