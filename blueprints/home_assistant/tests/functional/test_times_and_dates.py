e8375a35b34fb835554efc4dfb82a40e641c3806
vijay <vijay120@gmail.com>
2017-07-11 22:06:44 +0530

    added functional tests

--- blueprints/home_assistant/tests/functional/test_times_and_dates.py
+++ blueprints/home_assistant/tests/functional/test_times_and_dates.py
@@ -3,6 +3,7 @@ from conversational_test import ConversationalTest
 
 
 class TestSetAlarm(ConversationalTest):
+
     test_set_alarm_data = [
         ('set alarm for 6:15am this morning', '06:15:00'),
         ('wake me up tomorrow night at 9:32pm', '21:32:00'),
@@ -62,8 +63,9 @@ class TestSetAlarm(ConversationalTest):
                              "expected_retained_token, "
                              "list_remaining_alarms",
                              test_remove_alarm_data)
-    def test_cancel_only_one_alarm(self, set_query_1, set_query_2, cancel_query, expected_deleted_token,
-                                   expected_retained_token, list_remaining_alarms):
+    def test_cancel_only_one_alarm(self, set_query_1, set_query_2, cancel_query,
+                                   expected_deleted_token, expected_retained_token,
+                                   list_remaining_alarms):
         self.conv.say(set_query_1)
         self.conv.say(set_query_2)
         texts = self.conv.say(cancel_query)
@@ -71,44 +73,29 @@ class TestSetAlarm(ConversationalTest):
         self.assert_intent(self.conv, 'remove_alarm')
 
         texts = self.conv.say(list_remaining_alarms)
-        import pdb; pdb.set_trace()
-
         assert expected_retained_token in texts[0] and "removed" not in texts[0]
         self.assert_intent(self.conv, 'check_alarm')
 
-    test_remove_all_alarm_data = [
-        ('set alarm for 6:15am this morning',
-         'set alarm for 9:00pm',
-         'cancel all alarms',
-         '06:15:00',
-         '21:00:00',
-         'list all alarms'),
-        ('set alarm from 6:15am this morning',
-         'set alarm for 9:00pm',
-         'delete all alarms',
-         '06:15:00',
-         '21:00:00',
-         'list all alarms')
+    test_start_timer_data = [
+        ('remind me in 5 minutes', '5 minutes'),
+        ('activate a new timer', '60 seconds'),
+        ('let the timer begin', '60 seconds'),
     ]
 
-    @pytest.mark.parametrize("set_query_1, "
-                             "set_query_2, "
-                             "cancel_query, "
-                             "expected_deleted_token, "
-                             "expected_retained_token, "
-                             "list_remaining_alarms",
-                             test_remove_alarm_data)
-    def test_cancel_all_alarms(self, set_query_1, set_query_2, cancel_query, expected_deleted_token,
-                                   expected_retained_token, list_remaining_alarms):
-        self.conv.say(set_query_1)
-        self.conv.say(set_query_2)
-        texts = self.conv.say(cancel_query)
-        assert expected_deleted_token in texts[0] and "removed" in texts[0]
-        self.assert_intent(self.conv, 'remove_alarm')
+    @pytest.mark.parametrize("set_query, expected_response", test_start_timer_data)
+    def test_start_timer_data(self, set_query, expected_response):
+        texts = self.conv.say(set_query)
+        assert expected_response in texts[0]
+        self.assert_intent(self.conv, 'start_timer')
+        self.conv.say("clear timers")
 
-        texts = self.conv.say(list_remaining_alarms)
-        import pdb; pdb.set_trace()
+    test_clear_time_data = ['clear timer', 'stop my timers', 'pause the timer']
 
-        assert expected_retained_token in texts[0] and "removed" not in texts[0]
-        self.assert_intent(self.conv, 'check_alarm')
+    @pytest.mark.parametrize("clear_query", test_clear_time_data)
+    def test_clear_timer_data(self, clear_query):
+        self.conv.say('activate a new timer')
+        texts = self.conv.say(clear_query)
+        expected_response = 'The current timer has been cancelled'
+        assert expected_response in texts[0]
+
 
