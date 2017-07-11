bd7d1a5056f7afdd0ccb3883e72a9c472f1eb304
vijay <vijay120@gmail.com>
2017-07-11 23:16:18 +0530

    resolved merge conflicts

--- blueprints/home_assistant/tests/functional/test_times_and_dates.py
+++ blueprints/home_assistant/tests/functional/test_times_and_dates.py
@@ -2,7 +2,7 @@ import pytest
 from conversational_test import ConversationalTest
 
 
-class TestSetAlarm(ConversationalTest):
+class TestTimesAndDates(ConversationalTest):
     test_set_alarm_data = [
         ('set alarm for 6:15am this morning', '06:15:00'),
         ('wake me up tomorrow night at 9:32pm', '21:32:00'),
@@ -97,11 +97,15 @@ class TestSetAlarm(ConversationalTest):
         ('remind me in 5 minutes', '5 minutes'),
         ('activate a new timer', '60 seconds'),
         ('let the timer begin', '60 seconds'),
+        ('set a timer for 5 minutes', '5 minutes'),
+        ('start a timer for 5 mins', '5 minutes'),
+        ('start a timer for 40 seconds', '40 seconds'),
+        ('start a timer for 40 secs', '40 seconds')
     ]
 
     @pytest.mark.parametrize("set_query, expected_response",
                              test_start_timer_data)
-    def test_start_timer_data(self, set_query, expected_response):
+    def test_start_timer(self, set_query, expected_response):
         texts = self.conv.say(set_query)
         assert expected_response in texts[0]
         self.assert_intent(self.conv, 'start_timer')
@@ -110,10 +114,8 @@ class TestSetAlarm(ConversationalTest):
     test_clear_time_data = ['clear timer', 'stop my timers', 'pause the timer']
 
     @pytest.mark.parametrize("clear_query", test_clear_time_data)
-    def test_clear_timer_data(self, clear_query):
+    def test_clear_timer(self, clear_query):
         self.conv.say('activate a new timer')
         texts = self.conv.say(clear_query)
         expected_response = 'The current timer has been cancelled'
         assert expected_response in texts[0]
-
-
