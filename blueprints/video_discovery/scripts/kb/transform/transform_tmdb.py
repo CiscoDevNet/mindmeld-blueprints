c6bc697d1237feb01f2bd0db94e1a4209bfa332b
Ray Wang <juipin@cisco.com>
2017-07-13 17:27:12 -0700

    Remove missing release date and transform pop val with log

--- blueprints/video_discovery/scripts/kb/transform/transform_tmdb.py
+++ blueprints/video_discovery/scripts/kb/transform/transform_tmdb.py
@@ -1,16 +1,68 @@
+import json
+import logging
 import luigi
+import math
+import sys
 
 from .transform_movies import TransformMovies
 from .transform_tvs import TransformTVs
-from .transform_episodes import TransformEpisodes
+# from .transform_episodes import TransformEpisodes
+
+sys.path.append('..')
+from video_task import VideoDataProcessingTask  # noqa: F401
+from utils import load_jsonl  # noqa: F401
+
+
+class NormalizeTMDB(VideoDataProcessingTask):
+    output_dir = luigi.Parameter()
+
+    def requires(self):
+        return [TransformMovies(), TransformTVs()]
+
+    def output(self):
+        filename = u'transformed_videos.jsonl'
+        return self.get_output_target(filename)
+
+    def run(self):
+        # TODO: don't read it all into memory
+        movie_data = load_jsonl(self.input()[0])
+        tv_data = load_jsonl(self.input()[1])
+        video_data = self._filter(movie_data + tv_data)
+        video_data = self._normalize(video_data)
+        with self.output().open('w') as fp:
+            for obj in video_data:
+                line = '{}\n'.format(json.dumps(obj))
+                fp.write(line)
+
+    @staticmethod
+    def _filter(objs):
+        return [obj for obj in objs if obj.get('release_date')]
+
+    @staticmethod
+    def _normalize(objs):
+        popularity_max = max(obj['popularity'] for obj in objs)
+        popularity_min = min(obj['popularity'] for obj in objs)
+        logging.info('Get min /max for pop: {}/{}'.format(popularity_min, popularity_max))
+        for obj in objs:
+            obj['popularity'] = normalize(obj['popularity'], popularity_min, popularity_max)
+        return objs
+
+
+def normalize(val, min_val, max_val):
+    """
+    Normalize the popularity value with log transformation.
+    """
+    new_val = val - min_val + 1
+    return math.log(new_val)
 
 
 class TransformTMDB(luigi.WrapperTask):
     def requires(self):
-        return [TransformMovies(), TransformTVs()]
+        return NormalizeTMDB()
 
     def run(self):
-        yield TransformEpisodes()
+        # yield TransformEpisodes()
+        pass
 
 
 if __name__ == '__main__':
--- blueprints/video_discovery/scripts/kb/utils.py
+++ blueprints/video_discovery/scripts/kb/utils.py
@@ -11,6 +11,14 @@ def load_json(luigi_target):
         return json.load(fp)
 
 
+def load_jsonl(luigi_target):
+    objs = []
+    with luigi_target.open('r') as fp:
+        for line in fp:
+            objs.append(json.loads(line))
+    return objs
+
+
 def dump_json(luigi_target, obj):
     with luigi_target.open('w') as fp:
         json.dump(obj, fp, indent=4, sort_keys=True)
