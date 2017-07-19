410737df7149942b71a1c5bb737d4040cf14037a
jjjacksn <jj@mindmeld.com>
2017-07-18 17:10:08 -0700

    Template: Fix config

--- blueprints/template/config.py
+++ blueprints/template/config.py
@@ -4,31 +4,36 @@
 # The name of the application if it should be different from the app's enclosing directory name
 # APP_NAME = 'app-name'
 
-# A dict containing the configuration for the various NLP classifiers.
+# Dictionaries for the various NLP classifier configurations
 """
-CLASSIFIER_CONFIGS = {
-    'intent': { # An example decision tree model
-        'model_type': 'text',
-        'model_settings: {
-            'classifier_type': 'dtree'
+
+# An example decision tree model for intent classification
+INTENT_MODEL_CONFIG = {
+    'model_type': 'text',
+    'model_settings: {
+        'classifier_type': 'dtree'
+    },
+    'param_selection': {
+        'type': 'k-fold',
+        'k': 10,
+        'grid': {
+            'max_features': ['log2', 'sqrt', 0.01, 0.1]
         },
-        'param_selection': {
-            'type': 'k-fold',
-            'k': 10,
-            'grid': {
-                'max_features': ['log2', 'sqrt', 0.01, 0.1]
-            },
-            },
-            "features": {
-                "exact": {}
-            }
-        }
+    },
+    "features": {
+        "exact": {}
     }
 }
+
+# Fill in the other model configurations if necessary
+# DOMAIN_MODEL_CONFIG = {}
+# ENTITY_MODEL_CONFIG = {}
+# ROLE_MODEL_CONFIG = {}
 """
 
 # A example configuration for the parser
 """
+# *** Note: these are place holder entity types ***
 PARSER_CONFIG = {
     'grandparent': {
         'parent': {},
