7a293dfdb99a4b0ec097ca4762ceee996d681654
vembar <ace.varsha@gmail.com>
2017-10-09 16:08:52 -0700

    fixing timer issue

--- blueprints/home_assistant/app.py
+++ blueprints/home_assistant/app.py
@@ -138,11 +138,11 @@ def specify_location(context, responder):
             elif context['frame']['desired_action'] == 'Turn On Appliance':
                 selected_appliance = context['frame']['appliance']
                 reply = _handle_appliance_reply(selected_all, selected_location, selected_appliance,
-                                                desired_state="on", target_dialogue_state=None)
+                                                desired_state="on")
             elif context['frame']['desired_action'] == 'Turn Off Appliance':
                 selected_appliance = context['frame']['appliance']
                 reply = _handle_appliance_reply(selected_all, selected_location, selected_appliance,
-                                                desired_state="off", target_dialogue_state=None)
+                                                desired_state="off")
 
             del context['frame']['desired_action']
 
