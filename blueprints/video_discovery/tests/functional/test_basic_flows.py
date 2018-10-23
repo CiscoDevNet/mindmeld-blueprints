3a972b44e2b614fb2868e364144b1ff75ede2b37
jjjacksn <jdot@cisco.com>
2018-10-22 17:51:55 -0700

    Update video discovery for new package structure

--- blueprints/video_discovery/tests/functional/test_basic_flows.py
+++ blueprints/video_discovery/tests/functional/test_basic_flows.py
@@ -1,79 +1,79 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 import pytest
-from conversation_test import ConversationTest
-
-from mmworkbench import NaturalLanguageProcessor
-
-# Load the app
-app_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))[:-6]  # Remove tests dir
-nlp = NaturalLanguageProcessor(app_path)
-nlp.load()
 
 
-class TestBasicFlows(ConversationTest):
-    def test_greet(self):
-        response = nlp.process('hi')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'greet')
+def test_greet(convo):
+    convo.process('hi')
+    convo.assert_domain('video_content')
+    convo.assert_intent('greet')
 
-    def test_ask_content_single(self):
-        # TODO: complete the flow from doc
-        response = nlp.process('Can I see some movies with Tom Hanks?')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'browse')
 
-    def test_ask_content_multiple(self):
-        # TODO: complete the flow from doc
-        response = nlp.process('Can I see some movies with Tom Hanks?')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'browse')
+def test_ask_content_single(convo):
+    # TODO: complete the flow from doc
+    convo.process('Can I see some movies with Tom Hanks?')
+    convo.assert_domain('video_content')
+    convo.assert_intent('browse')
 
-        # TODO: hook up with context
-        response = nlp.process('Which ones were released in 2004?')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'browse')
 
-    def test_start_over(self):
-        # TODO: complete the flow from doc
-        response = nlp.process('start over')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'start_over')
+def test_ask_content_multiple(convo):
+    # TODO: complete the flow from doc
+    convo.process('Can I see some movies with Tom Hanks?')
+    convo.assert_domain('video_content')
+    convo.assert_intent('browse')
 
-    def test_exit(self):
-        # TODO: complete the flow from doc
-        response = nlp.process('bye')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'exit')
+    # TODO: hook up with context
+    convo.process('Which ones were released in 2004?')
+    convo.assert_domain('video_content')
+    convo.assert_intent('browse')
 
-    def test_help(self):
-        response = nlp.process('help me please')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'help')
 
-    def test_epg_time(self):
-        response = nlp.process('What time is The Big Bang Theory on?')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'unsupported')
+def test_start_over(convo):
+    # TODO: complete the flow from doc
+    convo.process('start over')
+    convo.assert_domain('video_content')
+    convo.assert_intent('start_over')
 
-    def test_epg_channel(self):
-        response = nlp.process('What channel is Games of Thrones on?')
-        self.assert_domain(response, 'video_content')
-        self.assert_intent(response, 'unsupported')
 
-    def test_insults(self):
-        response = nlp.process('You suck.')
-        self.assert_domain(response, 'unrelated')
-        self.assert_intent(response, 'insult')
+def test_exit(convo):
+    # TODO: complete the flow from doc
+    convo.process('bye')
+    convo.assert_domain('video_content')
+    convo.assert_intent('exit')
 
-    def test_compliments(self):
-        response = nlp.process('You are great!')
-        self.assert_domain(response, 'unrelated')
-        self.assert_intent(response, 'compliment')
 
-    @pytest.mark.skip(reason='Not implemented yet.')
-    def test_unrelated_general(self):
-        response = nlp.process('Get me a big mac')
-        self.assert_domain(response, 'unrelated')
-        self.assert_intent(response, 'insult')
+def test_help(convo):
+    convo.process('help me please')
+    convo.assert_domain('video_content')
+    convo.assert_intent('help')
+
+
+def test_epg_time(convo):
+    convo.process('What time is The Big Bang Theory on?')
+    convo.assert_domain('video_content')
+    convo.assert_intent('unsupported')
+
+
+def test_epg_channel(convo):
+    convo.process('What channel is Games of Thrones on?')
+    convo.assert_domain('video_content')
+    convo.assert_intent('unsupported')
+
+
+def test_insults(convo):
+    convo.process('You suck.')
+    convo.assert_domain('unrelated')
+    convo.assert_intent('insult')
+
+
+def test_compliments(convo):
+    convo.process('You are great!')
+    convo.assert_domain('unrelated')
+    convo.assert_intent('compliment')
+
+
+@pytest.mark.skip(reason='Not implemented yet.')
+def test_unrelated_general(convo):
+    convo.process('Get me a big mac')
+    convo.assert_domain('unrelated')
+    convo.assert_intent('insult')
--- blueprints/video_discovery/tests/functional/test_frame.py
+++ blueprints/video_discovery/tests/functional/test_frame.py
@@ -2,80 +2,68 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from mmworkbench.components import Conversation
-from mmworkbench.components import NaturalLanguageProcessor
-
-from conversation_test import ConversationTest
-
-APP_PATH = '../..'
-
-nlp = NaturalLanguageProcessor(app_path=APP_PATH)
-nlp.load()
+pytestmark = pytest.mark.frame
 
 
-@pytest.fixture()
-def convo():
-    return Conversation(nlp=nlp, app_path=APP_PATH)
+def test_action_movie(convo):
+    convo.say('action movies')
+    expected = {
+        'type': [{'type': 'type', 'text': 'movies', 'cname': 'movie'}],
+        'genre': [{'type': 'genre', 'text': 'action', 'cname': 'Action'}]
+    }
+    convo.assert_frame(expected)
 
 
-class TestFrame(ConversationTest):
-    @pytest.mark.frame
-    def test_action_movie(self, convo):
-        convo.say('action movies')
-        expected = {
-            'type': [{'type': 'type', 'text': 'movies', 'cname': 'movie'}],
-            'genre': [{'type': 'genre', 'text': 'action', 'cname': 'Action'}]
-        }
-        self.assert_frame(convo, expected)
+def test_comedy_tv(convo):
+    convo.say('i want to watch a funny show')
+    expected = {
+        'type': [{'type': 'type', 'text': 'show', 'cname': 'tv-show'}],
+        'genre': [{'type': 'genre', 'text': 'funny', 'cname': 'Comedy'}]
+    }
+    convo.assert_frame(expected)
 
-    def test_comedy_tv(self, convo):
-        convo.say('i want to watch a funny show')
-        expected = {
-            'type': [{'type': 'type', 'text': 'show', 'cname': 'tv-show'}],
-            'genre': [{'type': 'genre', 'text': 'funny', 'cname': 'Comedy'}]
-        }
-        self.assert_frame(convo, expected)
 
-    def test_jennifer_aniston_movie(self, convo):
-        convo.say('i want to watch a movie starring Jennifer Aniston')
-        expected = {
-            'type': [{'type': 'type', 'text': 'movie', 'cname': 'movie'}],
-            'cast': [{'type': 'cast', 'text': 'Jennifer Aniston', 'cname': None}]
-        }
-        self.assert_frame(convo, expected)
+def test_jennifer_aniston_movie(convo):
+    convo.say('i want to watch a movie starring Jennifer Aniston')
+    expected = {
+        'type': [{'type': 'type', 'text': 'movie', 'cname': 'movie'}],
+        'cast': [{'type': 'cast', 'text': 'Jennifer Aniston', 'cname': None}]
+    }
+    convo.assert_frame(expected)
 
-    def test_canadian_movie(self, convo):
-        convo.say('i want to watch a Canadian movie')
-        expected = {
-            'type': [{'type': 'type', 'text': 'movie', 'cname': 'movie'}],
-            # 'country': [{'type': 'country', 'text': 'Canadian'}]
-        }
-        self.assert_frame(convo, expected)
 
-    @pytest.mark.frame
-    def test_best_action_movie(self, convo):
-        convo.say('best action movies at 2002')
-        expected = {
-            'genre': [{'cname': 'Action', 'text': 'action', 'type': 'genre'}],
-            'sort': [{'cname': 'popular', 'text': 'best', 'type': 'sort'}],
-            'sys_time': [{
-                'cname': None, 'text': '2002', 'type': 'sys_time',
-                'value': {'grain': 'year', 'value': '2002-01-01T00:00:00.000-07:00'}
-            }],
-            'type': [{'cname': 'movie', 'text': 'movies', 'type': 'type'}]
-        }
-        self.assert_frame(convo, expected)
+def test_canadian_movie(convo):
+    convo.say('i want to watch a Canadian movie')
+    expected = {
+        'type': [{'type': 'type', 'text': 'movie', 'cname': 'movie'}],
+        # 'country': [{'type': 'country', 'text': 'Canadian'}]
+    }
+    convo.assert_frame(expected)
 
-    @pytest.mark.frame
-    def test_tv_range(self, convo):
-        convo.say('tv shows from the 90s')
-        expected = {
-            'sys_interval': [{'cname': None,
-                              'text': '90s',
-                              'type': 'sys_interval',
-                              'value': {'grain': 'year',
-                                        'value': ['1990-01-01T00:00:00.000-07:00',
-                                                  '2000-01-01T00:00:00.000-07:00']}}],
-            'type': [{'cname': 'tv-show', 'text': 'tv shows', 'type': 'type'}]
-        }
-        self.assert_frame(convo, expected)
+
+def test_best_action_movie(convo):
+    convo.say('best action movies at 2002')
+    expected = {
+        'genre': [{'cname': 'Action', 'text': 'action', 'type': 'genre'}],
+        'sort': [{'cname': 'popular', 'text': 'best', 'type': 'sort'}],
+        'sys_time': [{
+            'cname': None, 'text': '2002', 'type': 'sys_time',
+            'value': {'grain': 'year', 'value': '2002-01-01T00:00:00.000-07:00'}
+        }],
+        'type': [{'cname': 'movie', 'text': 'movies', 'type': 'type'}]
+    }
+    convo.assert_frame(expected)
+
+
+def test_tv_range(convo):
+    convo.say('tv shows from the 90s')
+    expected = {
+        'sys_interval': [{'cname': None,
+                          'text': '90s',
+                          'type': 'sys_interval',
+                          'value': {'grain': 'year',
+                                    'value': ['1990-01-01T00:00:00.000-07:00',
+                                              '2000-01-01T00:00:00.000-07:00']}}],
+        'type': [{'cname': 'tv-show', 'text': 'tv shows', 'type': 'type'}]
+    }
+    convo.assert_frame(expected)
