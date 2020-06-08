d9e22744da76215c9b2d85fe108792ad69c6d395
Lindsey Fiedler <lifiedle@cisco.com>
2020-06-08 13:37:45 -0500

    Minimum working product for screening app

---
+++ blueprints/screening_app/__init__.py
@@ -0,0 +1,421 @@
+from mindmeld import Application
+import screening_app.prediabetes as pd
+
+app = Application(__name__)
+
+@app.handle(intent='greet')
+def welcome(request, responder):
+    """
+    When the user begins the conversation with a greeting. Explain the system options.
+    """
+    responder.reply('Bienvenido al sistema de evaluación de salud. ' +
+    'Mediante unas sencillas preguntas, puedo ayudarte a determinar tu riesgo a padecer prediabetes. ' +
+    'Desea conocer su riesgo de padecer prediabetes?')
+
+@app.handle(intent='exit')
+def say_goodbye(request, responder):
+    """
+    When the user ends a conversation, clear the dialogue frame and say goodbye.
+    """
+    # Clear the dialogue frame to start afresh for the next user request.
+    responder.frame = {}
+
+    # Respond with a random selection from one of the canned "goodbye" responses.
+    responder.reply(['Gracias por su visita. Adios!', 'Gracias por su visita. Hasta luego!', 'Gracias por su visita. Que tenga buen día.'])
+    
+    
+@app.handle(intent='answer_no')
+def handle_rejection(request, responder):
+    """
+    When the user rejects the screening, gracefully exit the conversation.
+    """
+    # Clear the dialogue frame to start afresh for the next user request.
+    responder.frame = {}
+    
+    responder.reply(['Estamos para servirle. Gracias por su visita.'])
+
+
+@app.dialogue_flow(domain='prediabetes_screening', intent='answer_yes')
+def screen_prediabetes(request, responder):
+    """
+    If the user accepts the sceening, begin a dialogue flow, or if the user
+    answers yes to a question, save the answer and move to the next question.
+    """
+    if 'previous_question_number' not in responder.frame:
+        # Start questionnaire
+        
+        # Clear the dialogue frame to start fresh
+        responder.frame = {}
+        
+        first_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=pd.Q_AGE)
+    
+        responder.frame['previous_question'] = first_question[0]['text']
+        responder.frame['previous_question_number'] = first_question[0]['question_number']
+        responder.frame['screening'] = dict()
+    
+        responder.reply(first_question[0]['text'])
+        responder.listen()
+        
+        return
+    
+    previous_question = responder.frame['previous_question_number']
+    question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=previous_question)       
+
+    question_type = question[0]['type']
+    
+    if question_type == 'Binary':
+        responder.frame['screening'][previous_question] = True
+        responder.frame['count'] = 0
+            
+        try:
+            next_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=str(int(previous_question) + 1))
+            responder.frame['previous_question'] = next_question[0]['text']
+            responder.frame['previous_question_number'] = next_question[0]['question_number']
+            responder.reply(next_question[0]['text'])
+            responder.listen()
+            
+            return
+        except IndexError:
+            # No more questions. Reply with results.
+            risk = pd.calculate_risk_score(responder.frame['screening'])
+                
+            if risk >= 5:
+                responder.reply('Usted presenta un riesgo elevado de padecer de prediabetes y de desarrollar diabetes tipo 2.' +
+                    'Sólo un doctor puede darle un diagnóstico confiable por lo que recomendamos visite a su médico.')
+            else:
+                responder.reply('Su riesgo de padecer de prediabetes es bajo, sin embargo sólo un doctor puede darle un diagnóstico confiable.')
+                    
+            responder.exit_flow()  
+            return
+    else:
+        # Answer is out of questionnaire flow. Re-ask question.
+        question_text = responder.frame['previous_question']
+    
+    responder.frame['count'] = responder.frame.get('count', 0) + 1
+
+    if responder.frame['count'] <= 3:
+        responder.reply(question_text)
+        responder.listen()
+    else:
+        responder.reply('Disculpe, no le he podido entender. Por favor intente de nuevo.')
+        responder.exit_flow()
+    
+
+@screen_prediabetes.handle(intent='answer_no')
+def set_no_send_next(request, responder): 
+    """
+    When the user answers no to a question, save the answer and move to the next question.
+    """  
+    previous_question = responder.frame['previous_question_number']
+    question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=previous_question)
+
+    question_type = question[0]['type']
+    
+    if question_type == 'Binary':
+        responder.frame['screening'][previous_question] = False
+        responder.frame['count'] = 0
+            
+        try:
+            next_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=str(int(previous_question) + 1))
+            responder.frame['previous_question'] = next_question[0]['text']
+            responder.frame['previous_question_number'] = next_question[0]['question_number']
+            responder.reply(next_question[0]['text'])
+            responder.listen()
+            
+            return
+        except IndexError:
+            # No more questions. Reply with results.
+            risk = pd.calculate_risk_score(responder.frame['screening'])
+                
+            if risk >= 5:
+                responder.reply('Usted presenta un riesgo elevado de padecer de prediabetes y de desarrollar diabetes tipo 2.' +
+                    'Sólo un doctor puede darle un diagnóstico confiable por lo que recomendamos visite a su médico.')
+            else:
+                responder.reply('Su riesgo de padecer de prediabetes es bajo, sin embargo sólo un doctor puede darle un diagnóstico confiable.')
+                    
+            responder.exit_flow()  
+            return
+    else:
+        # Answer is out of questionnaire flow. Re-ask question.
+        question_text = responder.frame['previous_question']
+    
+    responder.frame['count'] = responder.frame.get('count', 0) + 1
+
+    if responder.frame['count'] <= 3:
+        responder.reply(question_text)
+        responder.listen()
+    else:
+        responder.reply('Disculpe, no le he podido entender. Por favor intente de nuevo.')
+        responder.exit_flow()
+ 
+ 
+@screen_prediabetes.handle(intent='answer_age')
+def set_age_send_next(request, responder):
+    """
+    When the user provides their age, save the answer and move to the next question.
+    """        
+    previous_question = responder.frame['previous_question_number']    
+    age_question = app.question_answerer.get(index='prediabetes_screening_questions', id=1)
+    
+    age_question_number = age_question[0]['question_number']
+    
+    if age_question_number is previous_question:
+        number_entity = next((e for e in request.entities if e['type'] == 'sys_number'), None)
+        if number_entity:
+            responder.frame['screening'][previous_question] = number_entity['value'][0]['value']
+            responder.frame['count'] = 0
+            
+            try:
+                next_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=str(int(previous_question) + 1))
+                responder.frame['previous_question'] = next_question[0]['text']
+                responder.frame['previous_question_number'] = next_question[0]['question_number']
+                responder.reply(next_question[0]['text'])
+                responder.listen()
+                
+                return
+            except IndexError:
+                # No more questions. Reply with results.
+                risk = pd.calculate_risk_score(responder.frame['screening'])
+                
+                if risk >= 5:
+                    responder.reply('Usted presenta un riesgo elevado de padecer de prediabetes y de desarrollar diabetes tipo 2.' +
+                        'Sólo un doctor puede darle un diagnóstico confiable por lo que recomendamos visite a su médico.')
+                else:
+                    responder.reply('Su riesgo de padecer de prediabetes es bajo, sin embargo sólo un doctor puede darle un diagnóstico confiable.')
+                    
+                responder.exit_flow()  
+                return
+        else:
+            # No age was provided. Re-ask question.
+            question_text = age_question[0]['text']
+    else:
+        # Answer is out of questionnaire flow. Re-ask question.
+        question_text = responder.frame['previous_question']
+    
+    responder.frame['count'] = responder.frame.get('count', 0) + 1
+
+    if responder.frame['count'] <= 3:
+        responder.reply(question_text)
+        responder.listen()
+    else:
+        responder.reply('Disculpe, no le he podido entender. Por favor intente de nuevo.')
+        responder.exit_flow()    
+
+  
+@screen_prediabetes.handle(intent='answer_gender')
+def set_gender_send_next(request, responder):
+    """
+    When the user provides their gender, save the answer and move to the next question.
+    """  
+    previous_question = responder.frame['previous_question_number']
+    gender_question = app.question_answerer.get(index='prediabetes_screening_questions', id=2)
+    
+    gender_question_number = gender_question[0]['question_number']
+    
+    if gender_question_number is previous_question:
+        gender_entity = next((e for e in request.entities if e['type'] == 'gender'), None)
+        if gender_entity:
+            responder.frame['screening'][previous_question] = gender_entity['value'][0]['cname']
+            responder.frame['count'] = 0
+            
+            try:
+                if gender_entity['value'][0]['cname'] == 'Mujer':
+                    next_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=str(int(previous_question) + 1))
+                else:
+                    next_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=str(int(previous_question) + 2))
+                
+                responder.frame['previous_question'] = next_question[0]['text']
+                responder.frame['previous_question_number'] = next_question[0]['question_number']
+                responder.reply(next_question[0]['text'])
+                responder.listen()
+                
+                return
+            except IndexError:
+                # No more questions. Reply with results.
+                risk = pd.calculate_risk_score(responder.frame['screening'])
+                
+                if risk >= 5:
+                    responder.reply('Usted presenta un riesgo elevado de padecer de prediabetes y de desarrollar diabetes tipo 2.' +
+                        'Sólo un doctor puede darle un diagnóstico confiable por lo que recomendamos visite a su médico.')
+                else:
+                    responder.reply('Su riesgo de padecer de prediabetes es bajo, sin embargo sólo un doctor puede darle un diagnóstico confiable.')
+                    
+                responder.exit_flow()  
+                return
+        else:
+            # No gender was provided. Re-ask question.
+            question_text = gender_question[0]['text']
+    else:
+        # Answer is out of questionnaire flow. Re-ask question.
+        question_text = responder.frame['previous_question']
+    
+    responder.frame['count'] = responder.frame.get('count', 0) + 1
+
+    if responder.frame['count'] <= 3:
+        responder.reply(question_text)
+        responder.listen()
+    else:
+        responder.reply('Disculpe, no le he podido entender. Por favor intente de nuevo.')
+        responder.exit_flow()  
+        
+
+@screen_prediabetes.handle(intent='answer_height')
+def set_height_send_next(request, responder):
+    """
+    When the user provides their height, save the answer and move to the next question.
+    """          
+    previous_question = responder.frame['previous_question_number']
+    height_question = app.question_answerer.get(index='prediabetes_screening_questions', id=7)
+    
+    height_question_number = height_question[0]['question_number']
+    
+    if height_question_number is previous_question:
+        number_entities = iter([e for e in request.entities if e['type'] == 'sys_number'])
+        unit_entity = next((e for e in request.entities if e['type'] == 'unit'), None)
+        
+        main_number = next(number_entities, None)
+        secondary_number = next(number_entities, None)
+        
+        if main_number:
+            # Convert to feet and inches if in meters
+            if unit_entity is None or len(unit_entity['value']) == 0 or unit_entity['value'][0]['cname'] in ['Metros', 'Centimetros']:
+                if secondary_number is None:
+                    number = main_number['value'][0]['value']
+                else:
+                    number = main_number['value'][0]['value'] + secondary_number['value'][0]['value'] / 100
+                height = pd.meters_to_feet(number)
+            else:
+                if secondary_number is None:
+                    height = str(main_number['value'][0]['value']) + '\'0"'
+                else:
+                    height = str(main_number['value'][0]['value']) + '\'' + str(secondary_number['value'][0]['value']) + '"'
+                
+            responder.frame['screening'][previous_question] = height
+            responder.frame['count'] = 0
+            
+            try:
+                next_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=str(int(previous_question) + 1))
+                responder.frame['previous_question'] = next_question[0]['text']
+                responder.frame['previous_question_number'] = next_question[0]['question_number']
+                responder.reply(next_question[0]['text'])
+                responder.listen()
+                
+                return
+            except IndexError:
+                # No more questions. Reply with results.
+                risk = pd.calculate_risk_score(responder.frame['screening'])
+                
+                if risk >= 5:
+                    responder.reply('Usted presenta un riesgo elevado de padecer de prediabetes y de desarrollar diabetes tipo 2.' +
+                        'Sólo un doctor puede darle un diagnóstico confiable por lo que recomendamos visite a su médico.')
+                else:
+                    responder.reply('Su riesgo de padecer de prediabetes es bajo, sin embargo sólo un doctor puede darle un diagnóstico confiable.')
+                    
+                responder.exit_flow()  
+                return
+        else:
+            # No height was provided. Re-ask question.
+            question_text = height_question[0]['text']
+    else:
+        # Answer is out of questionnaire flow. Re-ask question.
+        question_text = responder.frame['previous_question']
+    
+    responder.frame['count'] = responder.frame.get('count', 0) + 1
+
+    if responder.frame['count'] <= 3:
+        responder.reply(question_text)
+        responder.listen()
+    else:
+        responder.reply('Disculpe, no le he podido entender. Por favor intente de nuevo.')
+        responder.exit_flow()
+
+
+@screen_prediabetes.handle(intent='answer_weight')
+def set_weight_send_next(request, responder):
+    """
+    When the user provides their weight, save the answer and move to the next question.
+    """  
+    previous_question = responder.frame['previous_question_number']
+    weight_question = app.question_answerer.get(index='prediabetes_screening_questions', id=8)
+    
+    weight_question_number = weight_question[0]['question_number']
+    
+    if weight_question_number is previous_question:
+        number_entity = next((e for e in request.entities if e['type'] == 'sys_number'), None)
+        unit_entity = next((e for e in request.entities if e['type'] == 'unit'), None)
+        
+        if number_entity:
+            # Convert to lbs if in kilograms
+            if unit_entity is None or len(unit_entity['value']) == 0 or unit_entity['value'][0]['cname'] == 'Kilogramos':
+                weight = pd.kilos_to_pounds(number_entity['value'][0]['value'])
+            else:
+                weight = number_entity['value'][0]['value']
+            
+            responder.frame['screening'][previous_question] = weight
+            responder.frame['count'] = 0
+            
+            try:
+                next_question = app.question_answerer.get(index='prediabetes_screening_questions', question_number=str(int(previous_question) + 1))
+                responder.frame['previous_question'] = next_question[0]['text']
+                responder.frame['previous_question_number'] = next_question[0]['question_number']
+                responder.reply(next_question[0]['text'])
+                responder.listen()
+                
+                return
+            except IndexError:
+                # No more questions. Reply with results.
+                risk = pd.calculate_risk_score(responder.frame['screening'])
+                
+                if risk >= 5:
+                    responder.reply('Usted presenta un riesgo elevado de padecer de prediabetes y de desarrollar diabetes tipo 2.' +
+                        'Sólo un doctor puede darle un diagnóstico confiable por lo que recomendamos visite a su médico.')
+                else:
+                    responder.reply('Su riesgo de padecer de prediabetes es bajo, sin embargo sólo un doctor puede darle un diagnóstico confiable.')
+                    
+                responder.exit_flow()  
+                return
+        else:
+            # No age was provided. Re-ask question.
+            question_text = age_question[0]['text']
+    else:
+        # Answer is out of questionnaire flow. Re-ask question.
+        question_text = responder.frame['previous_question']
+    
+    responder.frame['count'] = responder.frame.get('count', 0) + 1
+
+    if responder.frame['count'] <= 3:
+        responder.reply(question_text)
+        responder.listen()
+    else:
+        responder.reply('Disculpe, no le he podido entender. Por favor intente de nuevo.')
+        responder.exit_flow()  
+
+
+@screen_prediabetes.handle(intent='answer_yes')
+def screen_prediabetes_in_flow_handler(request, responder):
+    screen_prediabetes(request, responder)
+    
+
+@screen_prediabetes.handle(intent='exit')
+def exit_questionnaire(request, responder):
+    """
+    When the user requests to cancel, say goodbye.
+    """  
+    say_goodbye(request, responder)
+            
+ 
+@screen_prediabetes.handle(default=True)
+def default_handler(request, responder):
+    responder.frame['count'] = responder.frame.get('count', 0) + 1
+    if responder.frame['count'] <= 3:
+        question_text = responder.frame['previous_question']
+        responder.reply(question_text)
+        responder.listen()
+    else:
+        responder.reply('Disculpe, no le he podido entender. Por favor intente de nuevo.')
+        responder.exit_flow()  
+        
+
+@app.handle(default=True)
+def default(request, responder):
+    welcome(request, responder)
---
+++ blueprints/screening_app/prediabetes.py
@@ -0,0 +1,131 @@
+Q_AGE = "1"
+Q_GENDER = "2"
+Q_GEST = "3"
+Q_FAM = "4"
+Q_BP = "5"
+Q_ACT = "6"
+Q_HEIGHT = "7"
+Q_WEIGHT = "8"
+
+one_pt_weight_cats = {
+    '4\'10"': (119,142),
+    '4\'11"': (124,147),
+    '5\'0"': (128,152),
+    '5\'1"': (132,157),
+    '5\'2"': (136,163),
+    '5\'3"': (141,168),
+    '5\'4"': (145,173),
+    '5\'5"': (150,179),
+    '5\'6"': (155,185),
+    '5\'7"': (159,190),
+    '5\'8"': (164,196),
+    '5\'9"': (169,202),
+    '5\'10"': (174,208),
+    '5\'11"': (179,214),
+    '6\'0"': (184,220),
+    '6\'1"': (189,226),
+    '6\'2"': (194,232),
+    '6\'3"': (200,239),
+    '6\'4"': (205,245)
+}
+
+two_pt_weight_cats = {
+    '4\'10"': (143,190),
+    '4\'11"': (148,197),
+    '5\'0"': (153,203),
+    '5\'1"': (158,210),
+    '5\'2"': (164,217),
+    '5\'3"': (169,224),
+    '5\'4"': (174,231),
+    '5\'5"': (180,239),
+    '5\'6"': (186,246),
+    '5\'7"': (191,254),
+    '5\'8"': (197,261),
+    '5\'9"': (203,269),
+    '5\'10"': (209,277),
+    '5\'11"': (215,285),
+    '6\'0"': (221,293),
+    '6\'1"': (227,301),
+    '6\'2"': (233,310),
+    '6\'3"': (240,318),
+    '6\'4"': (246,327)
+}
+
+three_pt_weight_cats = {
+    '4\'10"': 191,
+    '4\'11"': 198,
+    '5\'0"': 204,
+    '5\'1"': 211,
+    '5\'2"': 218,
+    '5\'3"': 225,
+    '5\'4"': 232,
+    '5\'5"': 240,
+    '5\'6"': 247,
+    '5\'7"': 255,
+    '5\'8"': 262,
+    '5\'9"': 270,
+    '5\'10"': 278,
+    '5\'11"': 286,
+    '6\'0"': 294,
+    '6\'1"': 302,
+    '6\'2"': 311,
+    '6\'3"': 319,
+    '6\'4"': 328
+}
+    
+def get_weight_category_score(height, weight):
+    if height not in one_pt_weight_cats:
+        # Height is out of measured range
+        return 0
+    
+    w = float(weight)
+    if one_pt_weight_cats[height][0] <= w <= one_pt_weight_cats[height][0]:
+        return 1
+    elif two_pt_weight_cats[height][0] <= w <= two_pt_weight_cats[height][0]:
+        return 2
+    elif three_pt_weight_cats[height] <= w:
+        return 3
+    else:
+        return 0
+        
+def meters_to_feet(meters):
+    feet = round(float(meters) // .3048)
+    inches = round(float(meters) / .3048 % 1 * 12)
+    
+    return str(feet) + '\'' + str(inches) + '"'
+    
+def kilos_to_pounds(weight):
+    return str(float(weight) * 2.2046)
+    
+def calculate_risk_score(answers):
+    score = 0
+    
+    # Add points for age
+    age = int(answers[Q_AGE])
+    if 40 <= age <= 49:
+        score += 1
+    elif 50 <= age <= 59:
+        score += 2
+    elif 60 <= age:
+        score += 3
+        
+    # Add a point if male
+    if answers[Q_GENDER] == "Hombre":
+        score += 1
+    elif answers[Q_GEST]:
+        # Add a point if had gestational diabetes
+        score += 1
+        
+    # Add a point if has family with diabetes or high blood pressure
+    score += sum([answers[Q_FAM], answers[Q_BP]])
+    
+    # Add point if not physically active
+    if not(answers[Q_ACT]):
+        score += 1
+        
+    # Add points for weight category
+    score += get_weight_category_score(answers[Q_HEIGHT], answers[Q_WEIGHT])
+    
+    return score
+    
+    
